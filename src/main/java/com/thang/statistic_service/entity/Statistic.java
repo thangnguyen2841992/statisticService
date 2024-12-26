170649678f17534e1b07b8d3fc7f9cfe4bf40d8a
thangnguyen1992 <ntthang4@cmcglobal.vn>
2024-12-26 09:34:42 +0700

    done

--- src/main/java/com/thang/statistic_service/entity/Statistic.java
+++ src/main/java/com/thang/statistic_service/entity/Statistic.java
@@ -7,9 +7,7 @@ import lombok.Setter;
 
 import java.util.Date;
 
-@Setter
-@Getter
-@Entity
+   @Entity
 @Table(name = "statistic")
 public class Statistic {
     @Id
@@ -23,4 +21,27 @@ public class Statistic {
     @Column(name = "created_date")
     private Date createdDate;
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
 }
