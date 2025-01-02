203bc087a5d9c8718c47a5eb7e35f44d9d03c166
thangnguyen2841992 <ntthang4@cmcglobal.vn>
2025-01-02 21:49:50 +0700

    done

--- src/main/java/com/thang/statistic_service/controller/StatisticRestController.java
+++ src/main/java/com/thang/statistic_service/controller/StatisticRestController.java
@@ -6,6 +6,7 @@ import com.thang.statistic_service.service.IStatisticService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
@@ -21,6 +22,7 @@ public class StatisticRestController {
     private IStatisticService statisticService;
 
     @PostMapping("/createNewStatistic.do")
+    @PreAuthorize("hasAuthority('SCOPE_log')")
     public StatisticDTO createNewStatistic(@RequestBody StatisticDTO statisticDTO) {
         logger.info("StatisticController: Add statistic");
 
@@ -33,7 +35,7 @@ public class StatisticRestController {
         statisticService.addStatistic(statisticDTO);
         return statisticDTO;
     }
-
+    @PreAuthorize("hasAuthority('SCOPE_read') && hasRole('ADMIN')")
     @GetMapping("/getAllStatistic.do")
     public List<StatisticDTO> getAllStatistic() {
         logger.debug("Get all statistic");
